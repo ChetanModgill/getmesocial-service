74af2ca402fa3598a600b8faa30e2cb1893050cd
ChetanModgill <Chetansharma.pu@gmail.com>
2023-01-15 22:34:42 -0500

    first api

---
+++ src/main/java/com/example/getmesocialservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.getmesocialservice.repository;
+
+import com.example.getmesocialservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    public User getUser(){
+        User user = new User("Chetan", "Canada", 27, "kferhgb");
+        return user;
+    }
+}
