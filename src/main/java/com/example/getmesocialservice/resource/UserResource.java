944fdd6364c3e429b37fc381d1a8eb179e3037cb
ChetanModgill <Chetansharma.pu@gmail.com>
2023-06-28 01:04:33 -0400

    url updated

--- src/main/java/com/example/getmesocialservice/resource/UserResource.java
+++ src/main/java/com/example/getmesocialservice/resource/UserResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "http://18.191.35.122:8080" )
+@CrossOrigin(origins = "http://localhost:4200" )
 public class UserResource {
     @Autowired
     private UserService userService;
